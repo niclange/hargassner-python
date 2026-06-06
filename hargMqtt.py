2c34fda3bb820c52b7119951582215292ac951db
Nicolas <thorynque@gmail.com>
2026-06-06 22:22:30 +0200

    correcition

--- hargMqtt.py
+++ hargMqtt.py
@@ -132,7 +132,7 @@ def registerData():
             elif n == 8:
                 metric_name = "tmp_fumee"
                 heater.tmp_fumee = data
-            elif n == 65:     
+            elif n == 64:     
                 metric_name = "reel_retour"
                 heater.reel_retour = data
             elif n == 24:     
